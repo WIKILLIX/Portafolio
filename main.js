b12bc266b18535a264ca437f156cb6163d2c71a2
brayanLaptop <ry-2585@hotmail.com>
2023-04-30 21:01:14 -0500

    section de contact y footer completas

--- main.js
+++ main.js
@@ -4,7 +4,9 @@ import Typed from "typed.js";
 const elemento = document.querySelector("#header-false");
 const header = document.querySelector("#header");
 const heroSection = document.querySelector("#hero-section");
-
+const skills = document.querySelector("#skills");
+const aboutMe = document.querySelector("#about-me");
+// valida si el nav sale de la vista del usuario o vuelve a entrar a la vista
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
@@ -19,7 +21,31 @@ const observer = new IntersectionObserver((entries) => {
 });
 
 observer.observe(elemento);
+// header-active
+//agrega la clase header-active al header basado en la sectin en la que se encuentre el usuario
+let options = {
+  root: document.querySelector("#main"),
+  rootMargin: "0px",
+  threshold: 1.0,
+};
 
+let observerMain = new IntersectionObserver(callback, options);
+
+var callback = function (entries, observer) {
+  entries.forEach((entry) => {
+    // Cada entry describe un cambio en la intersección para
+    // un elemento observado
+    //   entry.boundingClientRect
+    //   entry.intersectionRatio
+    //   entry.intersectionRect
+    //   entry.isIntersecting
+    //   entry.rootBounds
+    //   entry.target
+    //   entry.time
+  });
+};
+
+//efecto maquina de escribir
 const typed = new Typed(".text-presentation", {
   strings: ["web developer", "fullstack", "freelance"],
   typeSpeed: 50,
